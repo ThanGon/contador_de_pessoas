d838e2831abd042f55a230b2881f71e616819cea
ThanGon <nate.goncalves@gmail.com>
2022-02-22 17:38:46 -0300

    latest

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,51 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  @override 
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Container(
-        color: Colors.white10,
-        alignment: Alignment.center,
-        child: const Text('Olá Mundo!!!'),
-      ),
+    return const MaterialApp(
+      home: HomePage(),
     );
   }
 }
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [Text ('Tchau Nathan!')],
+      ),
+      appBar: AppBar(
+        backgroundColor: Colors.yellow,
+        title: Text ('Oi Nathan!'), 
+      ), 
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+/*class HomePage extends StatelessWidget {
+  const HomePage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Colors.white10,
+        alignment: Alignment.center,
+        child: const Text('Olá Mundo!'),
+    );
+  }
+}*/ // Salvar para futura referência OLÁ MUNDO
